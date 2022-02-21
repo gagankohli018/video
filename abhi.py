abc81a2465902904527f4838cbee3c2a7999d7a2
gagankohli018 <89466840+gagankohli018@users.noreply.github.com>
2022-02-21 08:46:56 +0530

    Create abhi.py
    https://github.com/gagankohli018/video.git
    ls
    cd video
    ls
    python2 abhi.py

---
+++ abhi.py
@@ -0,0 +1,46 @@
+---
+layout: page
+title: Video Lessons
+sidebar:
+  - title: "Learning Tracks"
+    links:
+      - text: "GAGAN420 for Newcomers"
+        url: /playlists/newcomers
+      - text: "Burp Suite"
+        url: /playlists/burp_suite
+      - text: "Pentest Series"
+        url: /playlists/pentesting_series
+      - text: "Web Hacking"
+        url: /playlists/web_hacking
+      - text: "Mentorship Mondays"
+        url: /playlists/mentorshipmondays
+        updated: true
+      - text: "Mobile Hacking"
+        url: /playlists/mobile_hacking
+      - text: "H@cktivitycon"
+        url: /playlists/hacktivitycon
+      - text: "Cryptography"
+        url: /playlists/cryptography
+  - title: "GAGAN420's Picks"
+    links:
+      - text: "A Starter's Guide to Pentesting with OWASP"
+        url: /sessions/pentest_owasp
+      - text: "JavaScript for Hackers"
+        url: /sessions/javascript_for_hackers
+      - text: "Server-Side Request Forgery"
+        url: /sessions/ssrf
+      - text: "Mobile Hacking Crash Course"
+        url: /sessions/mobile_crash_course
+      - text: Introduction to Docker Hacking 
+        url: /sessions/docker_hacking
+---
+
+These videos will help you learn a diverse set of topics. If you want to see specifics of what a session covers, simply click its title.
+
+<a class="btn btn-primary" href="/start-here">
+  Not sure where to start? Click here!
+</a>
+
+## Featured Lesson
+Check out [JavaScript for Hackers by STÖK](/sessions/javascript_for_hackers)!
+<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/FTeE3OrTNoA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
